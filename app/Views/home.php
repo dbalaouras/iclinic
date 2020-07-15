df3aff3cefbc0f89b8408852a583624e7f9ca780
Dimitris Balaouras <dbalaouras@gmail.com>
2020-07-15 23:01:09 +0300

    Fix title in homepage

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,4 +1,4 @@
-                        <h1 class="mt-4">Dashboard</h1>
+                        <h1 class="mt-4">Βασικές Λειτουργίες</h1>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
